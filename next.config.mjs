635be337f961962898a241768e5b90b2189ddbad
FAHIM <azmirhasan.az2@gmail.com>
2024-09-14 11:17:31 +0600

    Done

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'a0.muscache.com',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'glvmmupiqwlmhicmggqp.supabase.co',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'aduchqznppasfsslsvpx.supabase.co',
+        protocol: 'https',
+        port: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
